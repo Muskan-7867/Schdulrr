2d040c251de848a10ea536fe7320952ae47f6002
muskan <muskanloach984@gmail.com>
2024-10-04 15:39:22 +0530

    testimonials page

--- components/testimonials.jsx
+++ components/testimonials.jsx
@@ -1,5 +1,9 @@
+"use client"
+import Autoplay from "embla-carousel-autoplay";
 import { Card, CardContent } from "../components/ui/card"
 import {Carousel,CarouselContent,CarouselItem} from "../components/ui/carousel"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
 const testimonials = [
     {
       name: "Sarah Johnson",
@@ -33,17 +37,35 @@ const testimonials = [
 
 const Testimonials = () => {
   return (
-    <Carousel className="w-full max-w-sm">
-      <CarouselContent className='ml-1'>
-        {Array.from({ length: 5 }).map((_, index) => (
+    <Carousel 
+    plugins={[
+      Autoplay({
+        delay: 2000,
+      }),
+    ]}
+    className="w-full mx-auto">
+      <CarouselContent>
+        {testimonials.map((testimonial, index) => (
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
+              <Card className='h-full'>
+                <CardContent className="flex flex-col  h-full justify-between p-6">
+                  <p className="text-gray-600 mb-4">
+                     &quot;{testimonial.content}&quot;
+                  </p>
+                  <div className="flex items-center mt-4">
+                  <Avatar className="h-12 w-12 mr-4">
+                     <AvatarImage src={testimonial.image}
+                     alt={testimonial.name}/>
+                     <AvatarFallback>{testimonial.name.split(" ").map((n) => n[0]).join("")}</AvatarFallback>
+                  </Avatar>
+                 <div>
+                   <p className="font-semibold">{testimonial.name}</p>
+                   <p className="text-sm text-gray-500">{testimonial.role}</p>
+                 </div>
+                  </div>
                 </CardContent>
               </Card>
-            </div>
+            
           </CarouselItem>
         ))}
       </CarouselContent>

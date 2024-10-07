fbe9862065e3f4856c43856ff6be355f27c0cb26
muskan <muskanloach984@gmail.com>
2024-10-07 12:24:31 +0530

    usermenu

--- components/UserMenu.jsx
+++ components/UserMenu.jsx
@@ -1,26 +1,33 @@
-"use client"
-import { UserButton , UserButtonAction} from '@clerk/nextjs'
-import { ChartNoAxesColumn } from 'lucide-react'
-import React from 'react'
+"use client";
+import { UserButton, UserButtonAction } from '@clerk/nextjs';
+import { ChartNoAxesColumn } from 'lucide-react';
+import React from 'react';
 
 const UserMenu = () => {
   return (
     <UserButton
-    appearance={{
-        elements:{
-            avatarBox:"h-10 w-10",
+      appearance={{
+        elements: {
+          avatarBox: "h-10 w-10",
         },
-    }}
+      }}
     >
       <UserButton.MenuItems>
-        <UserButton.Link label='My Events'
-        href='/events'
-         labelIcon={<ChartNoAxesColumn size={15}/>}>
-     </UserButton.Link>
-        <UserButton.Action label="managAaccount"/>
+        <UserButton.Link 
+          label="My Events" 
+          href="/events" 
+          labelIcon={<ChartNoAxesColumn size={15} />}
+        />
+        <UserButton.Action 
+          label="Manage Account" 
+          onClick={() => {
+            // Add an action handler here (e.g., navigate or perform some action)
+            console.log('Manage Account clicked');
+          }}
+        />
       </UserButton.MenuItems>
     </UserButton>
-  )
+  );
 }
 
-export default UserMenu
+export default UserMenu;

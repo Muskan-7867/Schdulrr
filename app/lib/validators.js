15c295551d81779b026ce199188ddb3acc42dcd5
muskan <muskanloach984@gmail.com>
2024-10-08 14:35:43 +0530

    add eventschema

--- app/lib/validators.js
+++ app/lib/validators.js
@@ -12,4 +12,18 @@ export const usernameSchema = z.object({
 });
 
 
+ export const eventSchema =z.object({
+    title: z
+    .string()
+    .min(1, "Title is required")
+    .max(100,"Title must be 100 characters or less"),
+    description: z
+    .string()
+    .min(1, "Description is required")
+    .max(500,"Description must be 100 characters or less"),
+    duration: z.number().int().positive("Duration must be positive number"),
+
+    isPrivate: z.boolean(),
+})
+
 

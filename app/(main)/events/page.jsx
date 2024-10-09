c4edda9c32cb0c5ab3db5a4f9ec2f3b04f001c3f
muskan <muskanloach984@gmail.com>
2024-10-09 14:39:38 +0530

    eventpage

--- app/(main)/events/page.jsx
+++ app/(main)/events/page.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Suspense } from "react";
+import { getUserEvents } from "../../../actions/events";
+import EventCard from '../../../components/EventCard';
 
-const Eventpage = () => {
+export default function Eventspage(){
   return (
-    <div>
-      eventpage
-    </div>
-  )
+   <Suspense fallback={<div>Loading Events...</div>}>
+        <Events />
+   </Suspense>
+)}
+
+const Events = async () => {
+ const {events, username} =  await getUserEvents()
+ if(events.length === 0){
+  return <p>You does not created any events yet.</p>
+ }
+  return <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
+    {events.map((event)=>(
+      <EventCard Key={event.id} event={event}  username={username}/>
+    ))}
+  </div>
 }
 
-export default Eventpage
+

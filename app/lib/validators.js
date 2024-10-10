0ee0fa2f892d3d974d1d9e646b1542699fcbffee
muskan <muskanloach984@gmail.com>
2024-10-10 15:38:36 +0530

    add dayschema and availabilityschema

--- app/lib/validators.js
+++ app/lib/validators.js
@@ -27,3 +27,35 @@ export const eventSchema = z.object({
         .positive({ message: "Duration must be a positive number" }), 
     isPrivate: z.boolean(),
 });
+
+
+//day schema
+
+export const daySchema = z.object({
+    isAvailable: z.boolean(),
+    startTime: z.string().optional(),
+    endTime: z.string().optional(),
+})
+.refine(
+    (data) =>{
+        if(data.isAvailable){
+            return data.startTime <  data.endTime
+        }
+        return true;
+    },
+    {
+        message: "End time must be more than start Time.",
+        path:['endTime']
+    }
+);
+
+export const availabilitySchema= z.object({
+    monday: daySchema,
+    tuesday: daySchema,
+    wednesday: daySchema,
+    thursday: daySchema,
+    friday: daySchema,
+    saturday: daySchema,
+    sunday: daySchema,
+    timeGap: z.number().min(0,"Time gap must be 0 or more than minutes").int(),
+});

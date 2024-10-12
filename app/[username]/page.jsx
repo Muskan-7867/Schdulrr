cc18add72f34734273acfc60d02b17b6b05ec6b9
muskan <muskanloach984@gmail.com>
2024-10-12 22:49:05 +0530

    username page

--- app/[username]/page.jsx
+++ app/[username]/page.jsx
@@ -1,38 +1,17 @@
+// app/[username]/page.jsx
 import { notFound } from "next/navigation";
-import { getUserByUsername } from '../../actions/users'
-import { Avatar, AvatarImage, AvatarFallback }  from '../../components/ui/avatar'
-import { EventCard } from '../../components/EventCard';
+import { getUserByUsername } from '../../actions/users';
+import   UserProfile  from './UserProfile'; 
 
+const Userpage = async ({ params }) => {
+  const user = await getUserByUsername(params.username);
 
-const Userpage = async({params}) => {
-  const user = await getUserByUsername(params.username)
-  if(!user){
+  if (!user) {
     notFound();
   }
-  
-  return (
-    <div className="container mx-auto px-4 py-4">
-      <div className="flex flex-col items-center mb-8">
-        <Avatar className="h-24 w-24 mb-4">
-          <AvatarImage  src={user.imageUrl} alt={user.name}/>
-          <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
-        </Avatar>
-        <h1 className="text-3xl font-bold mb-2">{user.name}</h1>
-        <p className="text-gray-600 text-center">
-          Welcome to my scheduling page.Please select  an event below to book a  call with me. </p>
-      </div>
 
-      {user.events.length === 0  ? (
-        <p className="text-center text-gray-600">No public events available.</p>
-      ) : (
-        <div>{user.events.map((event) => {
-          return <EventCard />
-        }
-        )}</div>
-      
-      )}
-    </div>
-  )
-}
+  // Render the Client Component and pass the user data
+  return <UserProfile user={user} params={params} />;
+};
 
-export default Userpage
+export default Userpage;
